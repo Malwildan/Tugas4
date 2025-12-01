b623820450a747d3f42ad3100ac8a747a49c1877
Akmal Wildan <akmalwill26@gmail.com>
2025-12-02 00:27:31 +0700

    First commit: Implementasi kelas Mahasiswa dan Buku

---
+++ Mahasiswa.java
@@ -0,0 +1,39 @@
+package com.mb1.praktikum7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private List<Buku> daftarBuku;
+
+    public Mahasiswa(String nim, String nama) {
+        this.nim = nim;
+        this.nama = nama;
+        this.daftarBuku = new ArrayList<>();
+    }
+
+    public void pinjam(Buku buku) {
+        this.daftarBuku.add(buku);
+    }
+
+    public void tampilkanData() {
+        System.out.println("\n======================================================");
+        System.out.println("Nim  : " + this.nim);
+        System.out.println("Nama : " + this.nama);
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.printf("| %-2s | %-25s | %-50s |\n", "No", "Isbn", "Judul");
+        System.out.println("----------------------------------------------------------------------------------------");
+
+        if (daftarBuku.isEmpty()) {
+            System.out.printf("| %-2s | %-25s | %-50s |\n", "-", "Tidak ada buku", "-");
+        } else {
+            for (int i = 0; i < daftarBuku.size(); i++) {
+                Buku b = daftarBuku.get(i);
+                System.out.printf("| %-2d | %-25s | %-50s |\n", (i + 1), b.getIsbn(), b.getJudul());
+            }
+        }
+        System.out.println("----------------------------------------------------------------------------------------");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,43 @@
+package com.mb1.praktikum7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- APLIKASI PEMINJAMAN BUKU ---");
+        
+        System.out.print("Masukkan NIM Mahasiswa  : ");
+        String nim = scanner.nextLine();
+        
+        System.out.print("Masukkan Nama Mahasiswa : ");
+        String nama = scanner.nextLine();
+
+        Mahasiswa mahasiswa = new Mahasiswa(nim, nama);
+
+        boolean lanjut = true;
+        while (lanjut) {
+            System.out.println("\n--- Input Data Buku ---");
+            System.out.print("Masukkan No ISBN : ");
+            String isbn = scanner.nextLine();
+
+            System.out.print("Masukkan Judul   : ");
+            String judul = scanner.nextLine();
+
+            Buku bukuBaru = new Buku(isbn, judul);
+
+            mahasiswa.pinjam(bukuBaru);
+
+            System.out.print("Tambah buku lagi? (y/n): ");
+            String jawab = scanner.nextLine();
+            if (!jawab.equalsIgnoreCase("y")) {
+                lanjut = false;
+            }
+        }
+
+        mahasiswa.tampilkanData();
+        
+        scanner.close();
+    }
+}
